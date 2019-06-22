94c7364be7a2c90f1b25e687f0dd4c7f5cc66623
ViktorL <latkin.viktor@gmail.com>
2019-06-23 02:08:53 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// http://127.0.0.1:3000/echo?message=Hello -> Hello
+var http = require('http');
+var url = require('url');
+ 
+ 
+var server = new http.Server(function(req, res) {
+    console.log( req.method, req.url);
+ 
+    var urlParsed = url.parse(req.url);
+    console.log(urlParsed);
+    res.end("Hello, Irina");
+});
+ 
+//server.listen(3000, '127.0.0.1');
+server.listen(3000, '45.67.57.209');
